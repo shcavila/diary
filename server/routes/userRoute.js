61eea91872aea2cb3fc9cd90c884ca05d9b3b93f
avilash_sd2023@CEACCS.NET <avilash_sd2023@CEACCS.NET>
2019-11-19 17:48:05 +0800

    no refresh page not yet done no image

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,32 +4,63 @@ const Entry = require('../model/entry');
 
 
 
- router.route('/all').get((req, res) =>{
-     Entry.find({})
-     .then((doc) =>{
-        console.log('test')
-        //console.log(doc[0].img.toString('base64'))
-        res.json(doc)
-     })
-     .catch(err =>{
-        console.log(err)
-        res.status(400).json({err:err.message})
-     })
-
- });
-
- 
- router.route('/delete').post((req, res) =>{
-Entry.findByIdAndRemove(req.body.id)
-.then(doc =>{
-res.send(doc)
-console.log('result')
-console.log(doc)
-})
-.catch(err=>{
-   res.send(err)
-})
+router.route('/all').get((req, res) => {
+   Entry.find({})
+      .then((doc) => {
+         res.json(doc)
+      })
+      .catch(err => {
+         console.log(err)
+         res.status(400).json({ err: err.message })
+      })
 
 });
+
+
+router.route('/delete').post((req, res) => {
+   Entry.findByIdAndRemove(req.body.id)
+      .then(doc => {
+         if (doc) {
+            res.send('Successfully deleted')
+         } else {
+            res.end()
+         }
+      })
+      .catch(err => {
+         res.send(err)
+      })
+
+});
+
+router.route('/update').post((req, res) => {
+   Entry.findById(req.body.id)
+      .then(doc => {
+         if (doc) {
+            res.json(doc)
+         } else {
+            res.end()
+         }
+      })
+      .catch(err => {
+         res.send(err)
+      })
+
+});
+router.route('/update/:entryID').post((req, res) => {
+   console.log(req.params.entryID)
+   Entry.findById(req.body.id)
+      .then(doc => {
+         if (doc) {
+            res.json(doc)
+         } else {
+            res.end()
+         }
+      })
+      .catch(err => {
+         res.send(err)
+      })
+
+});
+
 
 module.exports = router;
